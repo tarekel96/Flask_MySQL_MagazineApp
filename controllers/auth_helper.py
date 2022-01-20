61739a62afdba3d3c83f7e94ad92c3b184ef82f3
Tarek <elhaj102@mail.chapman.edu>
2022-01-19 16:54:28 -0800

    made a login route for the admin user next will start making the frontend

--- controllers/auth_helper.py
+++ controllers/auth_helper.py
@@ -1,7 +1,12 @@
-# auth helper methods
-def admin_login(request):
-        user_pw = request.form.get('password')
-        if user_pw == "password":
-                return "Is authenticated."
-        else:
-                return "Error: Invalid password."
+from flask import Response
+
+# auth helper Class
+class auth_helper():
+
+        @staticmethod
+        def admin_login(request):
+                user_pw = request.form.get('password')
+                if user_pw == "password":
+                        return Response("{\n'message': 'Is Authenticated'\n}", status=201, mimetype='application/json')
+                else:
+                        return Response("{\n'message': 'Error - Invalid Password'\n}", status=401, mimetype='application/json')
--- routes/auth.py
+++ routes/auth.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, request
-from controllers.auth_helper import admin_login
+from controllers.auth_helper import auth_helper as auth
 
 auth_bp = Blueprint('auth', __name__)
 
 # tuple that stores bp name and url_prefix
 auth_bp_config = (auth_bp, "/auth")
 
-@auth_bp.route('/')
-def auth():
-        return "auth"
+@auth_bp.route('/', methods=['GET'])
+def index():
+        return "auth index"
 
 @auth_bp.route('/admin', methods=['POST'])
 def admin_auth():
         # user attempt to login as admin via form data
         if request.method == 'POST':
-                return admin_login(request)
+                return auth.admin_login(request)
