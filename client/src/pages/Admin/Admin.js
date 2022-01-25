24e74d6246d702061ade06226a7b4f3aa57f9b87
Tarek <elhaj102@mail.chapman.edu>
2022-01-25 00:47:25 -0800

    made the UI friendly in the admin page and laid out the different admin options into the form

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -4,26 +4,32 @@ import axios from 'axios';
 import styles from './admin.module.css';
 
 const Admin = () => {
+	const [ showForm, setFormVisibility ] = useState(true);
 	const [ content, setContent ] = useState([]);
 	const [ userSelection, setUserSelection ] = useState('view_all_mags');
 	const [ viewAllMags, setViewAllMags ] = useState(false);
 
 	const getAllMags = () => {
-		return axios
-			.get('http://127.0.0.1:5000/auth/magazine_catalog')
-			.then((res) => {
-				if (res.status === 201) {
-					return res;
-				}
-				console.log(res);
-			})
-			.then((json) => {
-				setContent(json.data);
-			})
-			.catch((e) => {
-				console.log(e);
-				alert(e);
-			});
+		try {
+			return axios
+				.get('http://127.0.0.1:5000/auth/magazine_catalog')
+				.then((res) => {
+					if (res.status === 201) {
+						return res;
+					}
+					console.log(res);
+				})
+				.then((json) => {
+					setContent(json.data);
+				})
+				.catch((e) => {
+					console.log(e);
+					alert(e);
+				});
+		} catch (e) {
+			console.log('An error occurred in fetching the data; see below:');
+			console.log(e);
+		}
 	};
 
 	const handleUserinput = (name) => {
@@ -45,24 +51,126 @@ const Admin = () => {
 		console.log(content);
 	};
 
+	const toggleForm = () => {
+		setFormVisibility((prevState) => !prevState);
+	};
+
+	const handleClear = () => {
+		setContent([]);
+	};
+
 	return (
 		<div>
-			<h1>Admin Page</h1>
-			<form onSubmit={(e) => handleSubmit(e)}>
-				<ol>
-					<li>
-						<label>View all magazines.</label>
-						<input
-							type="radio"
-							name="view_all_mags"
-							value={viewAllMags}
-							checked={viewAllMags}
-							onChange={(e) => handleUserinput(e.target.name)}
-						/>
-						<input type="submit" />
-					</li>
-				</ol>
-			</form>
+			<h1 className={styles['admin-header']}>Admin Page</h1>
+			{!showForm ? (
+				<div className={styles['show-btn-wrapper']}>
+					<button className={styles['show-btn']} onClick={toggleForm}>
+						Show Form
+					</button>
+				</div>
+			) : (
+				''
+			)}
+			{showForm ? (
+				<form className={styles['radio-form']} onSubmit={(e) => handleSubmit(e)}>
+					<span className={styles['form-hide-btn']} onClick={toggleForm}>
+						Hide
+					</span>
+					<h3>Options: </h3>
+					<ol>
+						<li>
+							<label htmlFor="view_all_mags">View all magazines.</label>
+							<input
+								type="radio"
+								name="view_all_mags"
+								value={viewAllMags}
+								checked={viewAllMags}
+								onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="view_all_cust">View all customers.</label>
+							<input
+								type="radio"
+								name="view_all_cust"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="mags_avg_cost_cat">
+								View average costs of magazines across categories.
+							</label>
+							<input
+								type="radio"
+								name="mags_avg_cost_cat"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="get_all_mags_by_year">View magazines by year.</label>
+							<input
+								type="radio"
+								name="get_all_mags_by_year"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="get_all_cust_by_city">View all customers by city.</label>
+							<input
+								type="radio"
+								name="get_all_cust_by_city"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="get_all_cust_prof">View all customer profiles.</label>
+							<input
+								type="radio"
+								name="get_all_cust_prof"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="get_all_mag_subs">View all magazine subscriptions.</label>
+							<input
+								type="radio"
+								name="get_all_mag_subs"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<li>
+							<label htmlFor="max_cat_by_zip">View top categories by zip code.</label>
+							<input
+								type="radio"
+								name="max_cat_by_zip"
+								// value={viewAllMags}
+								// checked={viewAllMags}
+								// onChange={(e) => handleUserinput(e.target.name)}
+							/>
+						</li>
+						<div className={styles['submit-btn-wrapper']}>
+							<input type="submit" />
+							<button type="button" className={styles['clear-btn']} onClick={handleClear}>
+								Clear
+							</button>
+						</div>
+					</ol>
+				</form>
+			) : (
+				''
+			)}
 			<div className={styles['grid-container']}>
 				<div className={styles['items-container']}>
 					{content.map((item) => (
